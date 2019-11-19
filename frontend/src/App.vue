87b880117688a7362c9586cbbb81563abdd14598
mickymck <mickymckeon@gmail.com>
2019-11-19 14:42:07 -0500

    welcome screen, code screen, and taste test name screen complete, minus code functionality

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,22 +1,41 @@
 <template>
   <div id="app">
-    <HostWelcome msg="Welcome to FLAVORDOME"/>
+    <!-- rendered component changes based on which one is dictated by fdComponent... moveToSetup method called by the listener on the setupScene event emitted on the Host button in HostWelcome -->
+    <component v-bind:is="fdComponent" @setupScene='moveToSetup' @cancelTestName='backToWelcome'></component>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import HostWelcome from './components/HostWelcome.vue'
+import TasteTestSetup from './components/TasteTestSetup.vue'
 
 export default {
   name: 'app',
   components: {
-    HostWelcome
+    HostWelcome,
+    TasteTestSetup
+  },
+  // this will be responsible for determining which component is currently rendered in the app. default is HostWelcome, but it can be toggled
+  data:() => {
+    return {
+      fdComponent: HostWelcome
+    }
+  },
+  // method that toggles the rendered component to TasteTestSetup. currently being used when Host button is clicked, but can re-use this for going Back to the beginning of the setup, if desired
+  methods: {
+    moveToSetup() {
+      this.fdComponent = TasteTestSetup
+    },
+    backToWelcome() {
+      this.fdComponent = HostWelcome
+    },
   }
 }
+
 </script>
 
 <style>
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -25,4 +44,9 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+button {
+  font-size: 32px;
+}
+
 </style>
---
+++ frontend/src/components/TasteTestSetup.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container" id="taste-test-setup">
+    <div>
+      <!-- if the taste test name has been entered, this will render -->
+      <h1 v-if="testNameLock">{{ testName }}</h1>
+    </div>
+
+    <!-- if the taste test name has not yet been submitted, you will have the field and buttons -->
+    <div v-if="!testNameLock">
+
+      <!-- the input field delivers testName, which I render in the header once it is submitted -->
+        <input type="input" v-model="testName" id="taste-test-name-field" placeholder="Taste Test Name" @keyup.enter="pushTestName" />
+        <div class='test-name-buttons'>
+
+          <!-- the submit button only displays when there is content in the field. once submit is clicked, pushTestName method is run, which locks the testName and renders it -->
+          <button id="submit-test-name-button" @click="pushTestName" v-if="testName">Submit</button>
+
+          <!-- if cancel button is clicked, cancelTestName is the event that our App.vue receives, and renders the HostWelcome page again -->
+          <button id="cancel-button" @click="cancelTestName">Cancel</button>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "TasteTestSetup",
+  props: {
+    testName: '',
+  },
+
+  // testNameLock determines whether the name renders or the form renders
+  data: () => {
+    return {
+    testNameLock: false
+    }
+  },
+
+  methods: {
+    // returns taster to the Welcome screen by toggling back to false
+    cancelTestName() {
+      this.$emit('cancelTestName');
+      },
+    pushTestName() {
+      this.testNameLock = true;
+      
+      }
+    }
+
+};
+
+</script>
+
+<style scoped>
+
+#taste-test-name-field {
+  font-size: 28px;
+  width: 80%;
+  height: 50px;
+  max-width: 400px;
+  text-align: center;
+  border: 4px solid #45c3f6;
+  color: #a600d8;
+  margin: 200px auto 40px auto;
+}
+
+.test-name-buttons {
+  display: flex;
+  flex-direction: column;
+  margin: 0 auto;
+}
+
+#submit-test-name-button {
+  margin-bottom: 40px;
+}
+
+</style>
