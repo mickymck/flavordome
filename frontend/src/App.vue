76aa2094462f18dc622494f730e24a1840a651a4
Ryan <rydowd@gmail.com>
2019-11-18 23:30:02 -0500

    getting pipenv and npm up to speed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to FLAVORDOME"/>
+    <ContestantsWrapper/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ContestantsWrapper from './components/ContestantsWrapper'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ContestantsWrapper
   }
 }
 </script>
