9cbcf075e81921dc059c599302eb6e46a6748166
Ryan <rydowd@gmail.com>
2019-11-21 12:02:14 -0500

    we have a test websocket with different functionality. working on getting channel layer up

--- config/routing.py
+++ config/routing.py
@@ -1,5 +1,11 @@
-from channels.routing import ProtocolTypeRouter
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import flavordome.routing
 
 application = ProtocolTypeRouter({
-  
+  'websocket':AuthMiddlewareStack(
+    URLRouter(
+      flavordome.routing.websocket_urlpatterns
+    )
+  )
 })
