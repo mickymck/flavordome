f24e159bb41e5ab8268d8f69159be2dde4aae213
Ryan <rydowd@gmail.com>
2019-12-02 17:48:54 -0500

    tweak sockets for dev environment

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -197,7 +197,7 @@ export const store = new Vuex.Store({
       const roomNum = Math.floor(100000 + Math.random()*900000)
       
       const newSocket = new WebSocket(
-        'wss://' + window.location.host +
+        'ws://' + window.location.host +
         '/ws/' + roomNum + '/'
       )
       newSocket.onopen = function(event){
@@ -210,7 +210,7 @@ export const store = new Vuex.Store({
     },
     joinSocket({commit, dispatch}, roomNum){
       const newSocket = new WebSocket(
-        'wss://'+ window.location.host + 
+        'ws://'+ window.location.host + 
         '/ws/' + roomNum + '/'
       )
       newSocket.onopen = function(event){
