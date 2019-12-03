cec3b94d7719cb7c663a4c4797f524d5bcd0c848
Ryan <rydowd@gmail.com>
2019-12-03 13:55:26 -0500

    tweaked for deployment

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -289,7 +289,7 @@ export const store = new Vuex.Store({
       const roomNum = Math.floor(100000 + Math.random()*900000)
       
       const newSocket = new WebSocket(
-        'ws://' + window.location.host +
+        'wss://' + window.location.host +
         '/ws/' + roomNum + '/'
       )
       newSocket.onopen = function(event){
@@ -302,7 +302,7 @@ export const store = new Vuex.Store({
     },
     joinSocket({commit, dispatch}, roomNum){
       const newSocket = new WebSocket(
-        'ws://'+ window.location.host + 
+        'wss://'+ window.location.host + 
         '/ws/' + roomNum + '/'
       )
       newSocket.onopen = function(event){
