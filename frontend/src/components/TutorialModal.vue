e0580da7b78f56ced5fb9116d3d27e38a86c8047
Ryan <rydowd@gmail.com>
2019-12-04 09:49:13 -0500

    implemented tutorial modal

---
+++ frontend/src/components/TutorialModal.vue
@@ -0,0 +1,51 @@
+<template>
+  <transition name='modal-fade'>
+    <div class='modal-backdrop'>
+      <div class='modal'>
+        <div class='tutorial'>
+          THE TUTORIAL GOES HERE
+        </div>
+        <button @click='closeModal'>Close</button>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name:'TutorialModal',
+  methods:{
+    closeModal(){
+      this.$emit('closeModal')
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .modal-backdrop{
+    position:fixed;
+    top:0;
+    left:0;
+    bottom:0;
+    right:0;
+    background-color:rgba(0,0,0,.35);
+    display:flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .modal{
+    background: #ffffff;
+    box-shadow: 2px 2px 20px 1px;
+    overflow-x: auto;
+    display:flex;
+    flex-direction: column;
+  }
+  .modal-fade-enter, .modal-fade-leave-active{
+    opacity: 0;
+  }
+  .modal-fade-enter-active, .modal-fade-leave-active{
+    transition: opacity .5s ease;
+  }
+  
+</style>
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -289,7 +289,7 @@ export const store = new Vuex.Store({
       const roomNum = Math.floor(100000 + Math.random()*900000)
       
       const newSocket = new WebSocket(
-        'wss://' + window.location.host +
+        'ws://' + window.location.host +
         '/ws/' + roomNum + '/'
       )
       newSocket.onopen = function(event){
@@ -302,7 +302,7 @@ export const store = new Vuex.Store({
     },
     joinSocket({commit, dispatch}, roomNum){
       const newSocket = new WebSocket(
-        'wss://'+ window.location.host + 
+        'ws://'+ window.location.host + 
         '/ws/' + roomNum + '/'
       )
       newSocket.onopen = function(event){
